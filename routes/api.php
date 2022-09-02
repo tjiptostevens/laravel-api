9c65aa2d9715197702e8c913fcc5099430c0085c
tjiptostevens <steveneonites@gmail.com>
2022-09-02 10:53:33 +0700

    feat: edit api.php

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/menus', [MenuController::class, 'store']);
+Route::get('/menus', [MenuController::class, 'index']);
