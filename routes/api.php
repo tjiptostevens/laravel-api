559a058061ea7cdab1ad73edaf8e9e5507e34f9c
tjiptostevens <steveneonites@gmail.com>
2022-09-02 20:12:00 +0700

    feat: add auth controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MenuController;
-use App\Models\Menu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/menus', [MenuController::class, 'store']);
+// Public routes
 Route::get('/menus', [MenuController::class, 'index']);
+Route::post('/register', [AuthController::class, 'register']);
+
+//Protected routes-Only authenticated users can have access to protected routes//
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Menu
+    Route::post('/menus', [MenuController::class, 'store']);
+    Route::put('/menus/{id}', [MenuController::class, 'update']);
+    Route::delete('/menus/{id}', [MenuController::class, 'delete']);
+});
